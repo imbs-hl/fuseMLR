e32ac091af5a9dcc8361c6c0978a9222007d9c0c
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2024-07-18 18:29:57 +0200

    For testing

--- tests/testthat/test-Data.R
+++ tests/testthat/test-Data.R
@@ -1,7 +1,7 @@
 data("entities")
 test_data <- Data$new(id = "geneexpr",
                       ind_col = "IDS",
-                      data_frame = entities$training$geneexpr[-10, ])
+                      data_frame = entities$training$geneexpr[-11, ])
 test_that("Data instantiates correctly", {
   expect_true(R6::is.R6(test_data))
   expect_equal(class(test_data)[1], "Data")
