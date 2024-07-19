8842ffae26e9650295bb59b1ebe1ff7eefd61646
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2024-07-19 07:12:45 +0200

    Param: 100% test coverage

--- tests/testthat/test-Param.R
+++ tests/testthat/test-Param.R
@@ -1,7 +1,16 @@
 ranger_param <- Param$new(id = "test_param",
                       param_list = list(probability = TRUE,
                                         mtry = 1))
-test_that("Param instantiates correctly", {
+test_that("Param: instantialize and print", {
   expect_true(R6::is.R6(ranger_param))
   expect_equal(class(ranger_param)[1], "Param")
+  print(ranger_param)
+})
+
+test_that("Param: getId", {
+  ranger_param$getId()
+})
+
+test_that("Param: getParamList", {
+  ranger_param$getParamList()
 })
