3f97324412cb6af04686eff4e0b3c7eb012ee0b9
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2024-11-25 16:59:58 +0100

    Regression and classification

--- R/weightedMeanLearner.R
+++ R/weightedMeanLearner.R
@@ -24,30 +24,34 @@
 weightedMeanLearner = function (x, y, weighted = TRUE) {
   # y must be binomial. If dichotomy, first category (case) = 1 and
   # second (control) = 0
-  if ((length(unique(y)) > 2) | is.character(y)) {
-    stop("y must be either binary or two level factor variable.")
+  if (is.numeric(y) & (length(unique(y)) > 2)) {
+    score_values = lapply(X = x, FUN = function (predicted) {
+      mean(x = (predicted - y)^2, na.rm = TRUE)
+    })
   } else {
-    if (!all(y %in% 0:1)) {
-      y = 1 - as.integer(y)
+    if ((length(unique(y)) > 2) | is.character(y)) {
+      stop("y must be either binary or two level factor variable.\n")
     } else {
       if (is.factor(y)) {
         y = as.integer(y) - 1
       } else {
-        y = y
+        if (!all(y %in% 0:1)) {
+          stop("y must take its values between 0 and 1.\n")
+        }
       }
+      score_values = lapply(X = x, FUN = function (predicted) {
+        mean(x = (predicted - y)^2, na.rm = TRUE)
+      })
     }
-    brier_values = lapply(X = x, FUN = function (predicted) {
-      mean(x = (predicted - y)^2, na.rm = TRUE)
-    })
-    brier_values = unlist(brier_values)
-    # weights_values = (1 - brier_values) / sum((1 - brier_values))
-    if (weighted) {
-      weights_values = (1 / brier_values) / sum((1 / brier_values))
-    } else {
-      weights_values <- 1 / length(brier_values)
-    }
-    names(weights_values) = names(x)
-    class(weights_values) = "weightedMeanLearner"
-    return(weights_values)
   }
+  score_values = unlist(score_values)
+  # weights_values = (1 - score_values) / sum((1 - score_values))
+  if (weighted) {
+    weights_values = (1 / score_values) / sum((1 / score_values))
+  } else {
+    weights_values <- rep(1 / length(score_values), length(score_values))
+  }
+  names(weights_values) = names(x)
+  class(weights_values) = "weightedMeanLearner"
+  return(weights_values)
 }
