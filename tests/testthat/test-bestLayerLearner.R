ad23de090a9a391b0469e88b6897dc30833530be
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2024-11-29 14:28:59 +0100

    cobra integrated

--- tests/testthat/test-bestLayerLearner.R
+++ tests/testthat/test-bestLayerLearner.R
@@ -33,5 +33,17 @@ test_that("bestLayerLearner works", {
     y = sample(x = c("0", "1"), size = 50L, replace = TRUE)
     my_model = bestLayerLearner(x = x, y = factor(y))
   })
+  expect_error(bestLayerLearner(x = data.frame(x1 = 1:3),
+                                y = c(1, 2, 1)))
+  expect_error(bestLayerLearner(x = data.frame(x1 = 1:3),
+                                y = as.character(c(1, 2, 1))))
+  expect_error(bestLayerLearner(x = data.frame(x1 = 1:3),
+                                y = as.factor(c(1, 2, 1,3,3))))
+
+  # Test 16: Expect error: x should be data.frame
+  expect_error(bestLayerLearner(x = matrix(1:9, nrow= 3, ncol= 3),
+                                y = sample(x = 0L:1L, size = 9L, replace = TRUE)))
+
+  expect_error(bestLayerLearner(x = list(x1 = 1:3), y = c(0, 1, 1)))
 })
 
