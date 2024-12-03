be9a02382529cb7399efb26abd6869c7f18881a7
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2024-12-03 12:25:50 +0100

    Update data example

--- test_code/build_data.R
+++ test_code/build_data.R
@@ -1,6 +1,6 @@
 library(InterSIM)
 library(this.path)
-set.seed(325)
+set.seed(3252)
 prop <- c(0.5, 0.5)
 effect <- 0.5
 source(file.path(dirname(this.path::this.path()), "myInterSIM.R"))
@@ -25,9 +25,10 @@ disease <- factor(as.integer(as.integer(train_sim_data$clustering.assignment$clu
 
 # Target data.frame
 target_df <- train_sim_data$clustering.assignment
+target_df$cluster.id <- as.integer(target_df$cluster.id == 2)
 names(target_df) <- c("IDS", "disease")
 target_df$IDS <- gsub(pattern = "subject",
-                      replacement = "patient",
+                      replacement = "participant",
                       x = target_df$IDS)
 
 index_split <- caret::createDataPartition(y = disease, p = 0.7)
@@ -46,7 +47,7 @@ subject_ids <- rownames(train_sim_data$dat.expr[index_split$Fold1, ])
 geneexpr_train_data <- as.data.frame(train_sim_data$dat.expr[index_split$Fold1[not_missing1], ])
 geneexpr_train_data$IDS <- subject_ids[not_missing1]
 geneexpr_train_data$IDS <- gsub(pattern = "subject",
-                      replacement = "patient",
+                      replacement = "participant",
                       x = geneexpr_train_data$IDS)
 # geneexpr_train_data$disease <- train_disease[not_missing1]
 geneexpr_train_data$disease <- NULL
@@ -57,7 +58,7 @@ not_missing2 <- sample(x = 1L:length(index_split$Fold1), size = 50L, replace = F
 proteinexpr_train_data <- as.data.frame(train_sim_data$dat.protein[index_split$Fold1[not_missing2], ])
 proteinexpr_train_data$IDS <- subject_ids[not_missing2]
 proteinexpr_train_data$IDS <- gsub(pattern = "subject",
-                                replacement = "patient",
+                                replacement = "participant",
                                 x = proteinexpr_train_data$IDS)
 proteinexpr_train_data$disease <- train_disease[not_missing2]
 proteinexpr_train_data$disease <- NULL
@@ -67,7 +68,7 @@ not_missing3 <- sample(x = 1L:length(index_split$Fold1), size = 50L, replace = F
 methylation_train_data <- as.data.frame(train_sim_data$dat.methyl[index_split$Fold1[not_missing3], ])
 methylation_train_data$IDS <- subject_ids[not_missing3]
 methylation_train_data$IDS <- gsub(pattern = "subject",
-                                   replacement = "patient",
+                                   replacement = "participant",
                                    x = methylation_train_data$IDS)
 methylation_train_data$disease <- train_disease[not_missing3]
 methylation_train_data$disease <- NULL
@@ -85,7 +86,7 @@ subject_ids <- rownames(train_sim_data$dat.expr[index_split$Fold2, ])
 geneexpr_test_data <- as.data.frame(train_sim_data$dat.expr[index_split$Fold2[test_not_missing1], ])
 geneexpr_test_data$IDS <- subject_ids[test_not_missing1]
 geneexpr_test_data$IDS <- gsub(pattern = "subject",
-                                   replacement = "patient",
+                                   replacement = "participant",
                                    x = geneexpr_test_data$IDS)
 # geneexpr_test_data$disease <- test_disease[test_not_missing1]
 geneexpr_test_data$disease <- NULL
@@ -94,7 +95,7 @@ test_not_missing2 <- sample(x = 1L:length(index_split$Fold2), size = 20L, replac
 proteinexpr_test_data <- as.data.frame(train_sim_data$dat.protein[index_split$Fold2[test_not_missing2], ])
 proteinexpr_test_data$IDS <- subject_ids[test_not_missing2]
 proteinexpr_test_data$IDS <- gsub(pattern = "subject",
-                               replacement = "patient",
+                               replacement = "participant",
                                x = proteinexpr_test_data$IDS)
 # proteinexpr_test_data$disease <- test_disease[test_not_missing2]
 proteinexpr_test_data$disease <- NULL
@@ -103,7 +104,7 @@ test_not_missing3 <- sample(x = 1L:length(index_split$Fold2), size = 20L, replac
 methylation_test_data <- as.data.frame(train_sim_data$dat.methyl[index_split$Fold2[test_not_missing3], ])
 methylation_test_data$IDS <- subject_ids[test_not_missing3]
 methylation_test_data$IDS <- gsub(pattern = "subject",
-                                  replacement = "patient",
+                                  replacement = "participant",
                                   x = methylation_test_data$IDS)
 # methylation_test_data$disease <- test_disease[test_not_missing3]
 methylation_test_data$disease <- NULL
@@ -112,7 +113,11 @@ test_entities <- list("geneexpr" = geneexpr_test_data,
                       "proteinexpr" = proteinexpr_test_data,
                       "methylation" = methylation_test_data,
                       "target" = target_df_test)
+train_entities$target$disease <- factor(train_entities$target$disease)
+test_entities$target$disease <- factor(test_entities$target$disease)
 entities = list(training = train_entities, testing = test_entities)
-save(entities, file = file.path(dirname(dirname(this.path::this.path())), "data/entities.rda"))
+multi_omics <- entities
+save(multi_omics, file = file.path(dirname(dirname(this.path::this.path())), "data/multi_omics.rda"))
 # Compress this large dataset.
-tools::resaveRdaFiles(paths = file.path(dirname(dirname(this.path::this.path())), "data/entities.rda"))
+tools::resaveRdaFiles(paths = file.path(dirname(dirname(this.path::this.path())), "data/multi_omics.rda"))
+
